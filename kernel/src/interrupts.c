2763c9edd6da8cf010344be036ffb25835de4581
Valerio Lomanto <valerio.lomanto@sns.it>
2022-02-28 01:03:42 +0100

    add syscall handler

--- kernel/src/interrupts.c
+++ kernel/src/interrupts.c
@@ -7,3 +7,7 @@ void interrupt_handler(usize raw_irq_number) {
 void exception_handler(usize raw_cause) {
     return;
 }
+
+void syscall_handler(usize arg1, usize arg2, usize arg3, usize arg4) {
+    return;
+}
