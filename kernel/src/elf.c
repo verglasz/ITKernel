10e5b91f756e5c169e1e2f4ebacd09276486c705
Valerio Lomanto <valerio.lomanto@sns.it>
2022-03-04 01:54:08 +0100

    add error msgs to elf.c and fix bug

--- kernel/src/elf.c
+++ kernel/src/elf.c
@@ -7,8 +7,8 @@
 
 static bool is_userspace(usize addr) {
     return (
-        USER_DATA_START <= addr && addr < USER_DATA_END && USER_PROG_START <= addr
-        && addr < USER_PROG_END);
+        USER_DATA_START <= addr && addr < USER_DATA_END
+        || USER_PROG_START <= addr && addr < USER_PROG_END);
 }
 
 /* Load an elf file from the eeprom into its memory
@@ -16,8 +16,19 @@ static bool is_userspace(usize addr) {
 EntryPoint elf_load_file(u16 addr) {
     Elf32_Ehdr elfheader;
     isize ret = eeprom_read(addr, &elfheader, sizeof(elfheader));
-    if (ret != 0 || __builtin_memcmp(elfheader.e_ident, ELFMAG, SELFMAG)) {
-        return NULL; // XXX: should probably distinguish the errors
+    if (ret != 0) {
+        serial_printf(" elf_load: eeprom read returned nonzero (%d)\n", ret);
+        return NULL;
+    }
+    if (__builtin_memcmp(elfheader.e_ident, ELFMAG, SELFMAG) != 0) {
+        char ident[5];
+        ident[0] = elfheader.e_ident[0];
+        ident[1] = elfheader.e_ident[1];
+        ident[2] = elfheader.e_ident[2];
+        ident[3] = elfheader.e_ident[3];
+        ident[4] = '\0';
+        serial_printf(" elf_load: memcmp of elf magic bytes failed, ident has %s:\n", ident);
+        return NULL;
     }
     // ideally we'd validate more than just the magic bytes, but time is what it is
     u16 ph_start = addr + elfheader.e_phoff;
@@ -28,6 +39,12 @@ EntryPoint elf_load_file(u16 addr) {
         if (ret != 0) return NULL;
         if (ph.p_type != PT_LOAD) continue;
         if (!is_userspace(ph.p_vaddr) || !is_userspace(ph.p_vaddr + ph.p_memsz - 1)) {
+            serial_printf(
+                " elf_load: segment requested address range outside userspace:"
+                "start at 0x%x, length 0x%x, ends at %x\n",
+                ph.p_vaddr,
+                ph.p_memsz,
+                ph.p_vaddr + ph.p_memsz);
             return NULL;
         }
         eeprom_read(addr + ph.p_offset, (u8 *)ph.p_vaddr, ph.p_filesz);
